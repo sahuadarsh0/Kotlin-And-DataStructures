ed7a2967ad117277395af2f2c7f0e3490c488ec9
sahuadarsh0 <sahuadarsh0@gmail.com>
2021-06-06 11:22:23 +0530

    Solution Added

--- src/main/kotlin/datastructures/easy/TwoSum.kt
+++ src/main/kotlin/datastructures/easy/TwoSum.kt
@@ -2,17 +2,26 @@ package datastructures.easy
 
 fun main() {
     val solution = Solution()
-    val ans1 = solution.twoSum(intArrayOf(2,7,11,15), 9)
-    val ans2 = solution.twoSum(intArrayOf(3,2,4), 6)
-    val ans3 = solution.twoSum(intArrayOf(3,3), 6)
-    ans1.forEach { println("ans1 $it") }
-    ans2.forEach { println("ans2 $it") }
-    ans3.forEach { println("ans3 $it") }
+    val ans1 = solution.twoSum(intArrayOf(2, 7, 11, 15), 9)
+    val ans2 = solution.twoSum(intArrayOf(3, 2, 4), 6)
+    val ans3 = solution.twoSum(intArrayOf(3, 3), 6)
+    print("ans1-> ")
+    ans1.forEach { print("$it") }
+    print("\nans2-> ")
+    ans2.forEach { print("$it") }
+    print("\nans3-> ")
+    ans3.forEach { print("$it") }
 }
 
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         lateinit var ans: IntArray
+        for ((indices, value) in nums.withIndex()) {
+            for (i in nums)
+                 if ((value + i) == target) {
+                     ans = intArrayOf(nums.indexOf(i),indices)
+                }
+        }
 
         return ans
     }
