49c4b5c4f4243955a1370c505e09925b6466744b
sahuadarsh0 <sahuadarsh0@gmail.com>
2021-07-11 05:35:07 +0530

    Added Loops When Experiment

--- src/main/kotlin/basics/When.kt
+++ src/main/kotlin/basics/When.kt
@@ -3,6 +3,7 @@ package basics
 fun main() {
 //    val day = readLine()!!
     val day = "Monday"
+
     when (day) {
         "Monday" -> {
             println("Today is Money Day")
@@ -17,4 +18,29 @@ fun main() {
             println("Working Day")
         }
     }
+
+    when (day) {
+        "Monday", "Sunday" -> println("day == Monday or day == Sunday")
+        else -> print("otherwise")
+    }
+
+    val x = 12+5+1
+    val y = 75+1
+    val validNumbers = 12..20
+    when (x) {
+        in 1..10 -> println("x is in the range")
+        in validNumbers -> println("x is valid $validNumbers")
+        !in 10..20 -> println("x is outside the range")
+        else -> print("none of the above")
+    }
+    when {
+        x.isOdd() -> print("x is odd") // Extension function
+        y.isEven() -> print("y is even")
+        else -> print("x+y is odd")
+    }
 }
+
+private fun Int.isEven(): Boolean = this % 2 == 0 // Extension function
+
+private fun Int.isOdd(): Boolean = this % 2 != 0 // Extension function
+
