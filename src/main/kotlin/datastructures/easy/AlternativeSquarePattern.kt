64f2e46ee9f6775a95f10b7f395ae98ea05be0e9
sahuadarsh0 <sahuadarsh0@gmail.com>
2021-08-08 12:00:00 -0600

    Added AlternativeSquarePattern.kt.kt Solution

--- src/main/kotlin/datastructures/easy/AlternativeSquarePattern.kt
+++ src/main/kotlin/datastructures/easy/AlternativeSquarePattern.kt
@@ -1,7 +1,26 @@
 package datastructures.easy
 
 fun main() {
+
+    asp(9)
+    asp(6)
+    asp(4)
 }
 
-class AlternativeSquarePattern {
+fun asp(num: Int) {
+    var k = 0
+    for (i in 1..num) {
+        if (i % 2 != 0) {
+            for (j in 1..5) {
+                print("${++k} ")
+            }
+        } else {
+            for (j in 5 downTo 1) {
+                print("${k + j} ")
+            }
+            k += 5
+        }
+        println()
+    }
+    println()
 }
